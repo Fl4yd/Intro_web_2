35ee0124506ff37da8f069ba2b0a598bd2c41adf
Jere Arokivi <51503069+Fl4yd@users.noreply.github.com>
2023-06-02 11:35:04 +0000

    first commit tehtavat 1-3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const table = document.getElementById("my-table");
+const username = document.getElementById("input-username");
+const email = document.getElementById("input-email");
+const address = document.getElementById("input-address");
+const admin = document.getElementById("input-admin");
+const submit = document.getElementById("submit-data");
+const empty_table = document.getElementById("empty-table");
+
+document.body.addEventListener("click", function (event) {
+  if (event.target === empty_table) {
+    const tbody = table.getElementsByTagName("tbody")[0];
+    while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+    }
+    console.log("Table emptied");
+  }
+
+  if (event.target === submit) {
+    var newRow = table.insertRow();
+    newRow.insertCell().innerHTML = username.value;
+    newRow.insertCell().innerHTML = email.value;
+    newRow.insertCell().innerHTML = address.value;
+    admin.checked
+      ? (newRow.insertCell().innerHTML = "X")
+      : (newRow.insertCell().innerHTML = "-");
+    console.log(
+      username.value +
+        ":" +
+        email.value +
+        ":" +
+        address.value +
+        ":" +
+        (admin.checked ? "X" : "-")
+    );
+  }
+});
